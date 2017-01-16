24f19b2f89b4e64adc9e0ce4b8453700b6bb355a
= <qkrtkdwls2121@gmail.com>
2017-01-16 10:20:53 +0900

    change settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rlti7#wu7iht4+)x_rj695e-+99(7aup!1wz@&b&^)*45xhp$m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
